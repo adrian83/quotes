a8dbcf4f2046ea9124d90258900672b0e1a77f14
adrian83 <adrianbrzoza1983@gmail.com>
2021-05-14 19:59:29 +0200

    [be] unit tests added

--- quotes-be/lib/domain/common/repository_test.dart
+++ quotes-be/lib/domain/common/repository_test.dart
@@ -23,16 +23,15 @@ void main() {
   ESStore<TestEntity> storeMock = TestEntityStoreMock();
   Repository<TestEntity> repository = Repository(storeMock, testEntityDecoder);
 
-  // void assertEntity(TestEntity expected, TestEntity actual) {
-  //   expect(expected.id, equals(actual.id));
-  //   expect(expected.name, equals(actual.name));
-  // }
+  void assertEntity(TestEntity expected, TestEntity actual) {
+     expect(expected.id, equals(actual.id));
+     expect(expected.name, equals(actual.name));
+   }
 
   test("save should persist entity", () async {
     // given
     var entity = TestEntity("abc-def", "Shakespear");
     var indexingResult = IndexResult("test-index", "entity", "abc-def", "INDEXED", 1);
-    // IndexResult(String index, String type, String id, this.result, this.version)
 
     when(() => storeMock.index(entity)).thenAnswer((_) => Future.value(indexingResult));
 
@@ -42,17 +41,17 @@ void main() {
     // then
     verify(() => storeMock.index(entity)).called(1);
   });
-/*
-  test("save should return failed Future if store throws exception", () async {
+
+  test("save should return failed Future if Store throws exception", () async {
     // given
     var entity = TestEntity("abc-def", "Shakespear");
-    when(storeMock.index(entity)).thenAnswer((_) => Future.error(StateError("exception")));
+    when(() => storeMock.index(entity)).thenAnswer((_) => Future.error(StateError("exception")));
 
     // when
-    expect(repository.save(entity), throwsStateError);
+    expect(repository.save(entity), throwsA(const TypeMatcher<StateError>()));
 
     // the
-    verify(storeMock.index(entity));
+    verify(() => storeMock.index(entity)).called(1);
   });
 
   test("find should return entity", () async {
@@ -62,15 +61,44 @@ void main() {
 
     var getResult = GetResult("author", "doc", "xyz-hij", 1, true, entityJson);
 
-    when(storeMock.get("abc-def")).thenAnswer((_) => Future.value(getResult));
+    when(() => storeMock.get(any())).thenAnswer((_) => Future.value(getResult));
 
     // when
     var result = await repository.find("abc-def");
 
     // then
-    verify(storeMock.get("abc-def"));
+    verify(() => storeMock.get(any())).called(1);
 
     assertEntity(entity, result);
   });
-  */
+  
+  test("find should return failed Future if Store throws exception", () async {
+    // given
+    when(() => storeMock.get(any())).thenAnswer((_) => Future.error(StateError("exception")));
+
+    // when
+    expect(repository.find("abc-poi"), throwsA(const TypeMatcher<StateError>()));
+
+    // the
+    verify(() => storeMock.get(any())).called(1);
+  });
+
+  test("update should update entity", () async {
+    // given
+    var entity = TestEntity("abc-def", "Shakespear");
+    var entityJson = "{\"id\": ${entity.id}, \"name\": ${entity.name}}";
+
+    var updateResult = UpdateResult("author", "doc", "abc-def", entityJson, 1);
+
+    when(() => storeMock.update(entity)).thenAnswer((_) => Future.value(updateResult));
+
+    // when
+    var result = await repository.update(entity);
+
+    // then
+    verify(() => storeMock.update(entity)).called(1);
+
+    assertEntity(entity, result);
+  });
+
 }
