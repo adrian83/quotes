3b02d4e1503259839ca04f1283c39acdc9282c53
adrian83 <adrianbrzoza1983@gmail.com>
2022-10-02 19:53:18 +0200

    [fe] update, cors enabled

--- quotesfe2/lib/domain/common/service.dart
+++ quotesfe2/lib/domain/common/service.dart
@@ -11,21 +11,37 @@ class Service<T> {
   // static final Logger logger = Logger('Service');
 
   static final _headers = {'Content-Type': 'application/json'};
+  
+  static final _corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+    "Access-Control-Allow-Headers": "*"
+  };
+
+  static final _allHeaders = {
+    ..._headers,
+    ..._corsHeaders,
+  };
 
   final Client http;
 
   Service(this.http);
 
-  Future<Map<String, dynamic>> createEntity(String url, T entity) =>
-      http.post(Uri.parse(url), headers: _headers, body: jsonEncode(entity)).then((response) => _handleErrors(response));
+  Future<Map<String, dynamic>> createEntity(String url, T entity) => http
+      .post(Uri.parse(url), headers: _allHeaders, body: jsonEncode(entity))
+      .then((response) => _handleErrors(response));
 
-  Future<Map<String, dynamic>> updateEntity(String url, T entity) =>
-      http.put(Uri.parse(url), headers: _headers, body: jsonEncode(entity)).then((response) => _handleErrors(response));
+  Future<Map<String, dynamic>> updateEntity(String url, T entity) => http
+      .put(Uri.parse(url), headers: _allHeaders, body: jsonEncode(entity))
+      .then((response) => _handleErrors(response));
 
-  Future<Map<String, dynamic>> getEntity(String url) => http.get(Uri.parse(url)).then((response) => _handleErrors(response));
+  Future<Map<String, dynamic>> getEntity(String url) => http
+      .get(Uri.parse(url), headers: _corsHeaders)
+      .then((response) => _handleErrors(response));
 
   Future<Map<String, dynamic>> deleteEntity(String url) =>
-      http.delete(Uri.parse(url)).then((response) => response.statusCode == 200 ? {} : _handleErrors(response));
+      http.delete(Uri.parse(url), headers: _corsHeaders).then((response) =>
+          response.statusCode == 200 ? {} : _handleErrors(response));
 
   Map<String, dynamic> _handleErrors(response) {
     if (response.statusCode == 404) {
@@ -57,7 +73,7 @@ class Service<T> {
     if (params.isEmpty) {
       return pair;
     }
-    
+
     return "$params&$pair";
   }
 }
