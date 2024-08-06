f2aa8c331631171d7859d13f5bab56233901aacf
adrian83 <adrianbrzoza1983@gmail.com>
2024-08-06 15:33:39 +0200

    minor improvements

--- quotes_common/example/quotes_common_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:quotes_common/quotes_common.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- quotes_common/test/quotes_common_test.dart
+++ quotes_common/test/quotes_common_test.dart
@@ -1,16 +1,19 @@
-import 'package:quotes_common/quotes_common.dart';
 import 'package:test/test.dart';
+
+import 'package:quotes_common/quotes_common.dart';
+import 'package:quotes_common/domain/author.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
+    final author = Author.create("Adam Mickiewicz", "Polish writer...");
 
     setUp(() {
       // Additional setup goes here.
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      expect(author.id, isNotNull);
+      expect(author.name, equals("Adam Mickiewicz"));
     });
   });
 }
